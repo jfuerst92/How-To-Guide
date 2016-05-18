3e703b669e5d3aee3a67cf910f6d301c5906d0fd
Joseph Fuerst <fuerstj@oregonstate.edu>
2016-05-18 09:59:56 -0700

    Update beer.js

--- beer.js
+++ beer.js
@@ -15,8 +15,11 @@ $(document).ready(function(){
 });
 ********************************************/
 
-//var base = "http://api.brewerydb.com/v2"
-var base = "http://jfuerst92.github.io/How-To-Guide/proxy.php"
+var base = "http://api.brewerydb.com/v2"
+//var base = "http://jfuerst92.github.io/How-To-Guide/proxy.php"
+//var type = "%26type%3dbeer";
+//var key = "%26key%3dd9e3c76540e2267dd4f9e09ede879957";
+//var route = "?route=%2fv2%2fsearch%3fq%3d";
 
 document.addEventListener('DOMContentLoaded', bind); //DOM must load first before binding buttons.
 
@@ -25,25 +28,25 @@ function bind(){
     el.addEventListener('click', function(event){
         var beerName = document.getElementById('br').value;
         var bts = beerName.split(' ').join('_');
-        var type = "%26type%3dbeer";
-        var key = "%26key%3dd9e3c76540e2267dd4f9e09ede879957";
-        var route = "?route=%2fv2%2fsearch%3fq%3d";
-        var fullUrl = base + route + bts + type + key; //construct a custom url based on the user's input
+        var type = "&type=beer";
+        var key = "&key=d9e3c76540e2267dd4f9e09ede879957";
+        var search = "/search?=;
+        var fullUrl = base + search + bts + type + key; //construct a custom url based on the user's input
         var req = new XMLHttpRequest(); //create the request with the new url
         req.open("GET", fullUrl, true);
         //req.setRequestHeader("Access-Control-Allow-Origin", "*");
         //req.setRequestHeader('Access-Control-Allow-Methods', 'GET');
         req.send(null);
        req.addEventListener('load', function(){
-           console.log("Request Recieved" + req.responseText);
-            //var response = JSON.parse(req.responseText); //parse the response text
-            //console.log(response);
-            //document.getElementById("sb").textContent = response.data[0].name;          //output the relevant data
-          //  document.getElementById("id").textContent = response.data[0].id;
-           // document.getElementById("try1").textContent = response.data[1].name;
-          //  document.getElementById("try2").textContent = response.data[2].name;
+           //console.log("Request Recieved" + req.responseText);
+            var response = JSON.parse(req.responseText); //parse the response text
+            console.log(response);
+            document.getElementById("sb").textContent = response.data[0].name;          //output the relevant data
+            document.getElementById("id").textContent = response.data[0].id;
+            document.getElementById("try1").textContent = response.data[1].name;
+            document.getElementById("try2").textContent = response.data[2].name;
             //more can be added, these are the minimum
-             event.preventDefault();
+            
         });
         
         //console.log(JSON.parse(req.responseText));
