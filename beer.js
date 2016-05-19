fba60794518aaa9cc227e4e0db233b22b165e2ef
Joseph Fuerst <fuerstj@oregonstate.edu>
2016-05-19 12:11:26 -0700

    Update beer.js

--- beer.js
+++ beer.js
@@ -31,6 +31,9 @@ function bind(){
         var type = "&type=beer";
         var key = "&key=d9e3c76540e2267dd4f9e09ede879957";
         var search = "/search?q=";
+        var beer = /beer/;
+        var id;
+        var brew = "?withBreweries=y"
         var fullUrl = base + search + bts + type + key; //construct a custom url based on the user's input
         var req = new XMLHttpRequest(); //create the request with the new url
         req.open("GET", fullUrl, true);
@@ -43,12 +46,23 @@ function bind(){
             console.log(response);
             document.getElementById("sb").textContent = response.data[0].name;          //output the relevant data
             document.getElementById("id").textContent = response.data[0].id;
+            id = response.data[0].id;
             document.getElementById("try1").textContent = response.data[1].name;
             document.getElementById("try2").textContent = response.data[2].name;
-            //more can be added, these are the minimum
+            fullUrl = base + beer + id + brew + key
             
         });
-        
+        req.open("GET", fullUrl, true);
+        //req.setRequestHeader("Access-Control-Allow-Origin", "*");
+        //req.setRequestHeader('Access-Control-Allow-Methods', 'GET');
+        req.send(null);
+        req.addEventListener('load', function(){
+           //console.log("Request Recieved" + req.responseText);
+            var response = JSON.parse(req.responseText); //parse the response tex
+            document.getElementById("brew").textContent = response.data.breweries[1].name; //output the relevant data
+           
+            
+        });
         //console.log(JSON.parse(req.responseText));
         event.preventDefault();
     });
