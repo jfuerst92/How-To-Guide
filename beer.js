d47f6f91bf5ffb31ff3618955d52db84bad7e735
Joseph Fuerst <fuerstj@oregonstate.edu>
2016-05-19 16:53:08 -0700

    Add files via upload

--- beer.js
+++ beer.js
@@ -1,14 +1,5 @@
-/******************
- * beer.js
- * by: Joseph Fuerst
- * 5/8/2016
- ******************/
-
 var base = "http://api.brewerydb.com/v2"
-//var base = "http://jfuerst92.github.io/How-To-Guide/proxy.php"
-//var type = "%26type%3dbeer";
-//var key = "%26key%3dd9e3c76540e2267dd4f9e09ede879957";
-//var route = "?route=%2fv2%2fsearch%3fq%3d";
+
 
 document.addEventListener('DOMContentLoaded', bind); //DOM must load first before binding buttons.
 
@@ -26,39 +17,19 @@ function bind(){
         //req.setRequestHeader("Access-Control-Allow-Origin", "*");
         //req.setRequestHeader('Access-Control-Allow-Methods', 'GET');
         req.send(null);
-        req.addEventListener('load', function(){
+       req.addEventListener('load', function(){
            //console.log("Request Recieved" + req.responseText);
             var response = JSON.parse(req.responseText); //parse the response text
-            document.getElementById("sb").textContent = response.data[0].name;
-            document.getElementById("beerImg").src = response.data[0].labels.medium;
+            console.log(response);
+            document.getElementById("sb").textContent = response.data[0].name;          //output the relevant data
             document.getElementById("id").textContent = response.data[0].id;
             document.getElementById("try1").textContent = response.data[1].name;
             document.getElementById("try2").textContent = response.data[2].name;
-            var beer = /beer/;
-            var brew = "?withBreweries=y"
-            var id = response.data[0].id;
-            var newUrl = base + beer + id + brew + key
-            var req2 = new XMLHttpRequest();
-            req2.open("GET", newUrl, true);
-            req2.send(null);
-            req2.addEventListener('load', function(){
-                var response2 = JSON.parse(req2.responseText); //parse the response tex
-                console.log(response2);
-                document.getElementById("brew").textContent = response2.data.breweries[0].name; 
-                var street = response2.data.breweries[0].locations[0].streetAddress;
-                var city = response2.data.breweries[0].locations[0].locality;
-                var state = response2.data.breweries[0].locations[0].region;
-                var zip = response2.data.breweries[0].locations[0].postalCode;
-                var fullAddr = street + ', ' + city + ', ' + state + ' ' + zip;
-                document.getElementById("loc").textContent = fullAddr;
-                document.getElementById("brewImg").src = response2.data.breweries[0].images.medium; 
-            
+            //more can be added, these are the minimum
+            event.preventDefault();
         });
-            
-        });
-       
+        
         //console.log(JSON.parse(req.responseText));
         event.preventDefault();
     });
 }
-    
